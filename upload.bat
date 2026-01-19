0c9e27cf73bbb720b057213736719e53ea06ca6d
ander-1028 <anderson921028@gmail.com>
2026-01-20 01:22:47 +0800

    Auto update 2026/01/20 週二  1:22:47.80

--- upload.bat
+++ upload.bat
@@ -2,6 +2,9 @@
 echo === 開始同步到 GitHub ===
 git add .
 git commit -m "Auto update %date% %time%"
-git push origin main
+
+:: 這裡改成 master 以符合你的設定
+git push origin master
+
 echo === 更新完成！Streamlit 正在重新部署 ===
 pause
